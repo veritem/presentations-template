4c5092084aa826c6871a98c368f9c98d53775916
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-30 12:12:33 +0200

    move to mdx-deck

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+  fonts: {
+    body: 'Poppins, sans-serif',
+    monospace: '"Poppins", monospace',
+  },
+  colors: {
+    text: 'white',
+    background: 'black',
+    primary: 'blue',
+  },
+}
